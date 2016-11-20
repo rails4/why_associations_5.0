4e182591092463e6a2979c2994573b27f9c5a2ab
Wlodek Bzyl <matwb@univ.gda.pl>
2016-11-20 17:45:50 +0100

    rubocop -a

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 1) do
-
-  create_table "customers", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'customers', force: :cascade do |t|
+    t.string   'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
