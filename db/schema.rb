d4ae9ff1926f9b109b8c8ad452db00c80482e965
Wlodek Bzyl <matwb@univ.gda.pl>
2016-12-04 08:33:29 +0100

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 5) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.string   "isbn"
+    t.string   "pub_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "articles_authors", id: false, force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "author_id"
+    t.index ["article_id"], name: "index_articles_authors_on_article_id"
+    t.index ["author_id"], name: "index_articles_authors_on_author_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "flights", id: false, force: :cascade do |t|
     t.text    "origin"
@@ -26,6 +54,15 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer "id"
     t.index ["dest"], name: "flights_dest"
     t.index ["origin"], name: "flights_origin"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "order_date"
+    t.string   "order_number"
+    t.integer  "customer_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
   create_table "sqlite_stat1", id: false, force: :cascade do |t|
