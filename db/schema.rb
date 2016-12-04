47761dcac3f5b8a9ea3390a8edd0a5e51bb7e927
Wlodek Bzyl <matwb@univ.gda.pl>
2016-12-04 08:26:11 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.string   "isbn"
-    t.string   "pub_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "articles_authors", id: false, force: :cascade do |t|
-    t.integer "article_id"
-    t.integer "author_id"
-    t.index ["article_id"], name: "index_articles_authors_on_article_id"
-    t.index ["author_id"], name: "index_articles_authors_on_author_id"
-  end
-
-  create_table "authors", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "customers", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "flights", id: false, force: :cascade do |t|
     t.text    "origin"
@@ -46,24 +18,14 @@ ActiveRecord::Schema.define(version: 5) do
     t.        "distance"
     t.        "dep_delay"
     t.        "arr_delay"
-    t.        "dep_time"
-    t.        "sched_dep_time"
-    t.        "arr_time"
-    t.        "sched_arr_time"
+    t.text    "dep_time"
+    t.text    "sched_dep_time"
+    t.text    "arr_time"
+    t.text    "sched_arr_time"
     t.        "air_time"
     t.integer "id"
     t.index ["dest"], name: "flights_dest"
-    t.index ["id"], name: "flights_id"
     t.index ["origin"], name: "flights_origin"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.datetime "order_date"
-    t.string   "order_number"
-    t.integer  "customer_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
   create_table "sqlite_stat1", id: false, force: :cascade do |t|
